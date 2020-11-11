b03386b0ce149345e484d50a646e338e6561e33e
philsco <philsco@gmail.com>
2020-11-11 06:07:21 -0800

    Initial commit

---
+++ ops.js
@@ -0,0 +1,67 @@
+const parseMap = (source, entity) => {
+    let raw = [];
+    source.forEach(item => {
+        raw.push(item[entity]);
+    });
+
+    let tempObj = raw.reduce((prev, next) => {
+        if (prev.hasOwnProperty(next)) {
+            prev[next] = prev[next]  + 1;
+            return prev; 
+        } else {
+            prev[next] = 1;
+            return prev;
+        }
+    }, {})
+
+    return Object.keys(tempObj).map(item => {
+        return [item, tempObj[item]];
+    })
+}
+
+const drawChart = (data, target) => {
+    let legend = [target, "Count"];
+    let elem = target.toLowerCase();
+    let parsed = parseMap(data, elem);
+    parsed.unshift(legend);
+
+    var source = google.visualization.arrayToDataTable(parsed);
+
+    var options = {title: target};
+
+    var chart = new google.visualization.PieChart(document.getElementById(elem));
+
+    chart.draw(source, options);
+  }
+
+  const drawTable = (data, elem) => {
+      let dates = new Map();
+      let source = [];
+      data.forEach(item => {
+          console.log(item.created)
+            if (item.created) {
+                if(dates.has(item.crated)) {
+                    dates.set(item.created, dates.get(item.created) + 1);
+                } else {
+                    dates.set(item.created, 1);
+                }
+            }
+        });
+        dates.forEach((val, key) => {
+            source.push([new Date(key), val]);
+        })
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({ type: 'date', id: 'Date' });
+        dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
+        dataTable.addRows(source);
+        let calendar = new google.visualization.Calendar(document.getElementById(elem));
+        var options = {
+            title: "Repo creation dates",
+            height: 350,
+          };
+        calendar.draw(dataTable, options);
+  }
+
+
+
+
---
+++ poller.js
@@ -0,0 +1,70 @@
+const Poller = (data, int, tag, rem, cyc) => {
+    var datasource = 'https://api.github.com/events'; 
+    var dataset = data || new Map();
+    var cycles = cyc || 0;
+    var interval = int || 10000;
+    var ETag = tag || "";
+    var remaining = rem || 60;
+
+    setTimeout(async () => {
+        fetch(datasource, {headers: {"If-None-Match": ETag}})
+        .then(resp => {
+        console.log('Got response')
+        console.log(resp.headers)
+        return {
+            data: resp.json(),
+            headers: resp.headers,
+            type: resp.type
+        }
+      })
+      .then(respObj => {
+          console.log("processing response")
+          interTmp = respObj.headers.get("x-poll-interval");
+          remaining = respObj.headers.get("X-Ratelimit-Remaining")
+          console.log("interval set to " + interval);
+          interval = (interTmp * 1000) +(60 - remaining) * 1000;
+          console.log("calls remaining " + remaining);
+          if (respObj.type === 304) {
+              return []
+          } else {
+              ETag = respObj.headers.get('etag').match(/".*"/)[0];
+              return respObj.data
+          }
+      })
+      .then(itemArr => {
+        return itemArr.map(item => { 
+          console.log(item.created_at)       
+          return {
+            id: item.id,
+            type: item.type,
+            created: item.created_at? item.created_at: null, 
+            comment: item.payload? item.payload.comments? item.payload.comments : null : null,
+            language: item.payload? item.payload.pull_request? item.payload.pull_request.head? item.payload.pull_request.head.repo
+            ? item.payload.pull_request.head.repo.language? item.payload.pull_request.head.repo.language
+            : null : null : null :null : null, repo: item.repo,
+          }
+          })
+        })
+        .then(parsed => {
+          parsed.forEach(item => {
+            dataset.set(item.id, item)
+            })
+        })
+        .then(() => {
+        cycles ++;
+        var event = new CustomEvent("refresh", {
+            detail: {
+              count: cycles,
+              size: dataset.size,
+              data: dataset
+            }
+        });
+        window.dispatchEvent(event);
+        Poller(dataset, interval, ETag, remaining, cycles);
+      })
+      .catch(err => {
+          console.log("ERR")
+          console.log(err)
+      })
+    }, interval)
+}
